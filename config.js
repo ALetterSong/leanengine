ae901a3991d44d62e8a2df187a0cf740d31a26b2
bigggge <me@haoduoyu.cc>
2016-10-21 16:47:06 +0800

    remove

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * config.js
+ *
+ * Created by xiepan on 2016/10/21 16:05.
+ */
+
+module.exports = {
+    // 'appid': 'wxc327caec4bc211b2',
+    'appid': 'wx643a898dde6521b6',//测试号
+    // 'secret': '1cdbca8ed5d35240963c9c324809bb28',
+    'secret': 'c9f1963739c1db5e4cd400a5a9077894',//测试号
+    'token': '545511611',
+    'encodingAESKey': 'OdLk1ccxRkYI0igkM3exGlV6SqOKWoQAYDhqrRo3vje'
+}
